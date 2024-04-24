848a49c86b016e08b96c0832239eb9b9573290ac
Nelson Espinal <nespinal@gmail.com>
2024-04-24 17:21:30 -0500

    POST method detection and JSON response

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
 // src/index.js
 var src_default = {
-  async fetch(request, env, ctx) {
-    if(request.url == "https://www.cloudflareworkers.com/test") {
-           return new Response('Hello worker!', {
-               headers: {
-                   'content-type': 'text/plain',
-               },
-           });
-       }
-       else{
-           return new Response('Error Worker! Wrong URL', {
-               headers: {
-                   'content-type': 'text/plain',
-               },
-           });
-       }
-  }
-};
-export {
-  src_default as default
-};
-//# sourceMappingURL=index.js.map
+	async fetch(request, env, ctx, options)
+									{
+	  if(request.method === 'POST' ) {
+			 //return new Response('Hello worker!'
+			 //^^old plain text response
+			 //New JSON response below
+			 return new Response(JSON.stringify({message: 'This is a POST request'}), {
+				 headers: {
+					 'content-type': 'application/json',
+				 },
+			 });
+		 }
+		 else{
+			//anything other than a POST method will receive this message in plain text
+			 return new Response('Error Worker! Wrong Method!', {
+				 headers: {
+					 'content-type': 'text/plain',
+				 },
+			 });
+		 }
+	}
+  };
+  export {
+	src_default as default
+  };
+  //# sourceMappingURL=index.js.map
