2f8631bc59a93fba60e5d55833748fbcf0a02574
Nelson Espinal <nespinal@gmail.com>
2024-04-24 16:29:28 -0500

    changes to index

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 // src/index.js
 var src_default = {
   async fetch(request, env, ctx) {
-    console.log("Logging: " + request.url)
-    return new Response("Hello World!");
+    if(request.url == "https://www.cloudflareworkers.com/test") {
+           return new Response('Hello worker!', {
+               headers: {
+                   'content-type': 'text/plain',
+               },
+           });
+       }
+       else{
+           return new Response('Error Worker! Wrong URL', {
+               headers: {
+                   'content-type': 'text/plain',
+               },
+           });
+       }
   }
 };
 export {
   src_default as default
 };
 //# sourceMappingURL=index.js.map
-
