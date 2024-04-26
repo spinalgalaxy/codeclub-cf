a9120eda33b1e02437e6fc2033da08a6cdbd0066
Nelson Espinal <nespinal@gmail.com>
2024-04-25 20:08:13 -0500

    Adding both a simple .json() method and another with JSON.stringify()

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var src_default = {
 	  if(request.method === 'POST' ) {
 			 //return new Response('Hello worker!'
 			 //^^old plain text response
-			 //New JSON response below
+			 //New JSON response below using JSON.stringify
 			 return new Response(JSON.stringify({message: 'This is a POST request'}), {
 				 headers: {
 					 'content-type': 'application/json',
@@ -14,13 +14,10 @@ var src_default = {
 		 }
 		 else{
 			//anything other than a POST method will receive this message in JSON format
-			 return new Response(JSON.stringify({message: 'Error Worker! Wrong Method!'}), {
-				 headers: {
-					 'content-type': 'text/plain',
-					//content-type doesn't seem to affect the output format
-				 },
-			 });
-		 }
+			//using the .json() method. It's a simpler way to return a value in JSON format
+			return Response.json({message: "ERROR! Wrong Method!!"});
+			
+		}
 	}
   };
   export {
