2b138738d9d2695e9afe185cb9394ade84e233b7
Nelson Espinal <nespinal@gmail.com>
2024-05-03 22:58:12 -0500

    Added random functions and a map variable

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 // src/index.js
 var src_default = {
 	async fetch(request, env, ctx, options) {
+		function getRandomInt(max) {
+			return Math.floor(Math.random() * max);
+		}
+		let rnd_num = getRandomInt(5);
+		const superPowerMap = {
+			0: "invisbility",
+			1: "bug-free coding :)",
+			2: "ultrasonic speed",
+			3: "levitation",
+			4: "x-ray vision",
+		};
+
 		if (request.method === 'GET') {
-			return new Response(JSON.stringify({ message: 'This is a GET request' }), {
-				headers: {
-					'content-type': 'application/json',
-				},
-			});
+			return Response.json({ message: "This is your random superpower: " + superPowerMap[rnd_num] });
 		}
 		else {
 
@@ -18,4 +26,3 @@ var src_default = {
 export {
 	src_default as default
 };
-//# sourceMappingURL=index.js.map
