e0d07be51788e6857fa354ca3b2dc1fd4173b546
Nelson Espinal <nespinal@gmail.com>
2024-05-03 22:12:43 -0500

    simplified code

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
 // src/index.js
 var src_default = {
-	async fetch(request, env, ctx, options)
-									{
-	  if(request.method === 'POST' ) {
-			 //return new Response('Hello worker!'
-			 //^^old plain text response
-			 //New JSON response below using JSON.stringify
-			 return new Response(JSON.stringify({message: 'This is a POST request'}), {
-				 headers: {
-					 'content-type': 'application/json',
-				 },
-			 });
-		 }
-		 else{
-			//anything other than a POST method will receive this message in JSON format
-			//using the .json() method. It's a simpler way to return a value in JSON format
-			return Response.json({message: "ERROR! Wrong Method!!"});
-			
+	async fetch(request, env, ctx, options) {
+		if (request.method === 'GET') {
+			return new Response(JSON.stringify({ message: 'This is a GET request' }), {
+				headers: {
+					'content-type': 'application/json',
+				},
+			});
+		}
+		else {
+
+			return Response.json({ message: "ERROR! Wrong Method!!" });
+
 		}
 	}
-  };
-  export {
+};
+export {
 	src_default as default
-  };
-  //# sourceMappingURL=index.js.map
+};
+//# sourceMappingURL=index.js.map
