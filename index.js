4e59222ea5365ff4685cfaf511e09e6ab88527ed
Nelson Espinal <nespinal@gmail.com>
2024-05-13 17:03:31 -0500

    removed old assignment #2 code

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
 // src/index.js
 var src_default = {
 	async fetch(request, env, ctx, options) {
-		function getRandomInt(max) {
-			return Math.floor(Math.random() * max);
-		}
-		let rnd_num = getRandomInt(5);
-		const superPowerMap = {
-			0: "invisbility",
-			1: "bug-free coding :)",
-			2: "ultrasonic speed",
-			3: "levitation",
-			4: "x-ray vision",
-		};
+
 		//evaluate correct method and likely human score >= 30
 		//use the cf.botManagement.score property to obtain the score
 		//this could also be added to a variable [e.g.: const bot_score = request.cf.botManagement.score]
